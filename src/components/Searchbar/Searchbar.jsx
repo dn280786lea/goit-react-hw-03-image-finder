ea96ada00aea15acf8731f8cc8c56013ddc2a410
dn280786lea <ekaterina.lubenets@gmail.com>
2023-11-19 19:55:51 +0100

    Update Searchbar.jsx
    ьь

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,14 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 export class Searchbar extends Component {
   state = {
     q: '',
   };
+
   handleChange = evt => {
     this.setState({ q: evt.target.value });
   };
+
   handleSubmit = evt => {
     evt.preventDefault();
     this.props.onSubmit(this.state.q);
@@ -15,11 +17,12 @@ export class Searchbar extends Component {
   render() {
     return (
       <header className="searchbar">
-        <form className="form">
+        <form className="form" onSubmit={this.handleSubmit}>
           <button type="submit" className="button">
             <span className="button-label">Search</span>
           </button>
           <input
+            onChange={this.handleChange}
             placeholder="search"
             name="search"
             className="input"
